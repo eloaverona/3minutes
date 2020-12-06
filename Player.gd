e97518007976d1b3df6f7d36bf148c38b7499f71
Eloá França Verona <eloafverona@gmail.com>
2020-12-06 12:42:54 -0300

    Changed player moviment

--- Player.gd
+++ Player.gd
@@ -1,11 +1,10 @@
 extends KinematicBody2D
 
 
-export var SPEED = Vector2(150.0, 350.0)
+export var SPEED = Vector2(100.0, 100.0)
 onready var GRAVITY = ProjectSettings.get("physics/2d/default_gravity")
 onready var platform_detector = $PlatformDetector
 onready var animation_player = $AnimationPlayer
-onready var shoot_timer = $ShootAnimation
 onready var sprite = $AnimatedSprite
 
 const FLOOR_NORMAL = Vector2.UP
@@ -15,21 +14,10 @@ var velocity = Vector2.ZERO
 
 
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
+	velocity.y += delta * GRAVITY * 1.5 ## player is affected by 
+	
 	$AnimatedSprite.play()
-#
-#	if Input.is_action_pressed("ui_left"):
-#		velocity.x = -WALK_SPEED
-#	elif Input.is_action_pressed("ui_right"):
-#		velocity.x =  WALK_SPEED
-#	else:
-#		velocity.x = 0
-#
-#	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-#
-#	# The second parameter of "move_and_slide" is the normal pointing up.
-#	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
-#	move_and_slide(velocity, Vector2(0, -1))
+	
 	var direction = get_direction()
 
 	var is_jump_interrupted = Input.is_action_just_released("ui_up") and velocity.y < 0.0
@@ -44,20 +32,16 @@ func _physics_process(delta):
 	)
 
 	# When the character’s direction changes, we want to to scale the Sprite accordingly to flip it.
-	# This will make Robi face left or right depending on the direction you move.
-
-
+	# This will make the character face left or right depending on the direction you move.
 
 	if direction.x != 0:
 		$AnimatedSprite.flip_v = false
 		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
-		#sprite.flip_v = false if direction.x > 0 else -1
-		
-	var animation = get_new_animation(false)
-	$AnimatedSprite.animation = animation
 
-	
+		
+	var animation = get_new_animation()
+	$AnimatedSprite.animation = animation
 
 
 
@@ -80,18 +64,19 @@ func calculate_move_velocity(
 	velocity.x = speed.x * direction.x
 	if direction.y != 0.0:
 		velocity.y = speed.y * direction.y
-	if is_jump_interrupted:
-		# Decrease the Y velocity by multiplying it, but don't set it to 0
-		# as to not be too abrupt.
-		velocity.y *= 0.6
+		#print(velocity)
+#	if is_jump_interrupted:
+#		# Decrease the Y velocity by multiplying it, but don't set it to 0
+#		# as to not be too abrupt.
+#		velocity.y *= 0.9
+#	print(velocity)
 	return velocity
 
 
-func get_new_animation(is_shooting = false):
+func get_new_animation():
 	var animation_new = ""
 	if is_on_floor():
 		animation_new = "run" if abs(velocity.x) > 0.1 else "idle"
 	else:
-		animation_new = "jump"
-	
+		animation_new = "fall" if velocity.y > 0 else "jump"
 	return animation_new
