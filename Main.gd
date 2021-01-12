2b8c90b8534cca39b9df70af90a5ac840d416ca4
Eloá França Verona <eloafverona@gmail.com>
2021-01-12 08:51:16 -0300

    Initial pass at adding other people that are running to game
    Its very rough still

---
+++ Main.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var spawned = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	spawn_people()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	if(!spawned):
+#		spawn_people()
+#		spawned = true
+
+func spawn_people():
+	var scene = load("res://Person.tscn")
+	for n in range(9):
+		var person = scene.instance()
+		add_child(person)
