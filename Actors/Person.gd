005162597f25abb571bcaf19491b68f27cf7cc56
Eloá França Verona <eloafverona@gmail.com>
2021-02-02 08:34:11 -0300

    Refactor Person move to stop when array is complete

--- Actors/Person.gd
+++ Actors/Person.gd
@@ -13,45 +13,59 @@ var right_index = 0
 var jump_index = 0
 var left_index = 0
 var read = false
+var hasRightMove = false
+var hasLeftMove = false
+var hasJumpMove = false
 
 func initialize(right, left, jump):
 	toTheRight = right
 	toTheLeft = left
 	toJump = jump
+	if(toTheRight.size() > 0):
+		hasRightMove = true
+	if(toTheLeft.size() > 0):
+		hasLeftMove = true
+	if(toJump.size() > 0):
+		hasJumpMove = true
 	
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta): 
-	
-	var st_jump = toJump[jump_index]
-	var jump = st_jump != 'False'
-	
-	var st = toTheRight[right_index]
-	if(st == ''):
-		right_index = 0
-		st = toTheRight[right_index]
-	
-	var st_left = toTheLeft[left_index]
-	if(st_left == ''):
-		left_index = 0
-		st_left = toTheLeft[left_index]
-	
-	var moveRightStrenght = int(toTheRight[right_index])
-	var moveLeftStrenght = int(toTheLeft[left_index])
-	
-	right_index += 1 
-	left_index += 1 
-	jump_index += 1 
-	
-	if(right_index >= toTheRight.size()):
-		right_index = 0
-	if(left_index >= toTheLeft.size()):
-		left_index = 0
-	if(jump_index >= toJump.size()):
-		jump_index = 0
-
-
-	# actor move is defined in the Actor class
-	actor_move(jump, moveRightStrenght, moveLeftStrenght)
+	var canMove = hasLeftMove || hasRightMove || hasJumpMove
+	if(canMove):
+		var st_jump = toJump[jump_index]
+		var jump = st_jump != 'False'
+		
+		var st = toTheRight[right_index]
+		if(st == ''):
+			right_index = 0
+			st = toTheRight[right_index]
+		
+		var st_left = toTheLeft[left_index]
+		if(st_left == ''):
+			left_index = 0
+			st_left = toTheLeft[left_index]
+		
+		var moveRightStrenght = int(toTheRight[right_index])
+		var moveLeftStrenght = int(toTheLeft[left_index])
+		
+		if(hasRightMove):
+			right_index += 1 
+		if(hasLeftMove):
+			left_index += 1
+		if(hasJumpMove):
+			jump_index += 1 
+		
+		if(right_index >= toTheRight.size()):
+			right_index = 0
+			hasRightMove = false
+		if(left_index >= toTheLeft.size()):
+			left_index = 0
+			hasLeftMove = false
+		if(jump_index >= toJump.size()):
+			jump_index = 0
+			hasJumpMove = false
+		# actor move is defined in the Actor class
+		actor_move(jump, moveRightStrenght, moveLeftStrenght)
 
 func read_moves():
 	pass
